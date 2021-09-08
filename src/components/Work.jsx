dbe6c1fdb9f8c528c2f005f88268f144f599d49f
Sai Anirudh <b.anirudh10@gmail.com>
2021-09-09 03:35:56 +0530

    Fixed some css

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,17 +1,30 @@
-import classes from "./styles/work.module.css";
+import { useState, useEffect } from "react";
 import Card from "./ui/Card";
+import classes from "./styles/work.module.css";
+
+const projects = [
+  {
+    title: "Algorithm Visualizer",
+    summary:
+      "This is a project which I have built for visualizing all the famous algorithms out there to see and understand deeply how they actually work.",
+    url: "/",
+  },
+];
 
 function Work() {
-  const projects = [
-    {
-      title: "Algorithm Visualizer",
-      summary:
-        "This is a project which I have built for visualizing all the famous algorithms out there to see and understand deeply how they actually work.",
-      url: "/",
-    },
-  ];
+  const [transform, setTransform] = useState({});
+  useEffect(() => {
+    const transition = setTimeout(() => {
+      setTransform({ transform: "none", opacity: 1 });
+    }, 300);
+
+    return () => {
+      clearTimeout(transition);
+    };
+  }, []);
+
   return (
-    <section className={classes["work-container"]}>
+    <section className={classes["work-container"]} style={transform}>
       <h1 className={classes["work-heading"]}>.projects</h1>
       <div className={classes.projects}>
         <Card data={projects[0]} className={classes["project-1"]} />
